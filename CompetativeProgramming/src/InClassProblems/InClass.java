ccc5a7589ce7b20251c0e29f13422225c100a6cb
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-02-06 19:41:13 -0600

    Heap Practice

--- CompetativeProgramming/src/InClassProblems/InClass.java
+++ CompetativeProgramming/src/InClassProblems/InClass.java
@@ -1,2 +1,36 @@
-package InClassProblems;public class InClass {
+package InClassProblems;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class InClass {
+
+    public static void main (String[] args){
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        pq.add(1);
+        pq.add(2);
+        pq.add(4);
+        pq.add(3);
+        pq.add(0);
+        pq.add(2);
+
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+
+        System.out.println(pq);
+        System.out.println("test");
+
+        PriorityQueue<Pair> pq1 = new PriorityQueue<>();
+        Pair p1 = new Pair('a',1);
+        Pair p2 = new Pair('b',2);
+        Pair p3 = new Pair('c',3);
+        Pair p4 = new Pair('d',4);
+
+
+
+    }
+
 }
--- CompetativeProgramming/src/InClassProblems/Pair.java
+++ CompetativeProgramming/src/InClassProblems/Pair.java
@@ -1,2 +1,59 @@
-package InClassProblems;public class Pair {
+package InClassProblems;
+
+public class Pair implements Comparable<Pair>{
+
+    char firstValue;
+    int secondValue;
+
+    public Pair(char firstValue, int secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    public char getFirstValue() {
+        return firstValue;
+    }
+
+    public void setFirstValue(char firstValue) {
+        this.firstValue = firstValue;
+    }
+
+    public int getSecondValue() {
+        return secondValue;
+    }
+
+    public void setSecondValue(int secondValue) {
+        this.secondValue = secondValue;
+    }
+
+
+//    private Long id;
+//    private String name;
+//    private LocalDate dob;
+//
+//    @Override
+//    public int compareTo(Employee o)
+//    {
+//        return this.getId().compareTo( o.getId() );
+//    }
+
+    @Override
+    public int compareTo(Pair p) {
+
+        if (this.getSecondValue() > p.secondValue) {
+            return 1;
+        } else if ( this.getSecondValue() == p.secondValue) {
+            return 0;
+        } else {
+            return -1;
+        }
+
+//        int a = this.getSecondValue();
+//        int b = p.getSecondValue();
+//
+//        return a.compareTo(b);
+
+        //return this.getSecondValue().compareTo( p.getSecondValue() );
+        //return 0;
+    }
 }
