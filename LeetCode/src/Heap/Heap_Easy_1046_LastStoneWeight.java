97a54aee95978d941b5e853ed50440ec93aadc82
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-02-12 17:17:27 -0600

    Heap_Easy_1046_LastStoneWeight

--- LeetCode/src/Heap/Heap_Easy_1046_LastStoneWeight.java
+++ LeetCode/src/Heap/Heap_Easy_1046_LastStoneWeight.java
@@ -1,25 +1,10 @@
+package Heap;
+
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.PriorityQueue;
 
-public class main {
-
-    public static void main (String args[]) {
-        //int[] array = {4,5,6,7,0,1,2};
-        //int[] array = {11,13,15,17};
-        //[11,13,15,17]
-        //[9,3,2,10]
-        int[] array = {9,3,2,10};
-        int answer = lastStoneWeight(array);
-
-        System.out.print(answer);
-
-
-
-
-    }
-
-    private static int lastStoneWeight(int[] stones) {
+public class Heap_Easy_1046_LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
 
         if (stones.length == 1) {
             return stones[0];
@@ -28,7 +13,7 @@ public class main {
         int answer = -1;
 
         //put the stones in a MAX heap
-        PriorityQueue <Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int s: stones) {
             pq.add(s);
         }
@@ -39,9 +24,8 @@ public class main {
             int b = pq.poll();
 
             //if they are = do nothing
-            if (a == b) {
-                continue;
-            } else {
+
+            if (a != b) {
                 int n = a-b;
                 pq.add(n);
             }
@@ -51,15 +35,9 @@ public class main {
         if (pq.isEmpty()) {
             answer = 0;
         } else {
-            while (pq.size() > 1) {
-                pq.poll();
-            }
             answer = pq.poll();
         }
 
         return answer;
     }
-
-
 }
-
