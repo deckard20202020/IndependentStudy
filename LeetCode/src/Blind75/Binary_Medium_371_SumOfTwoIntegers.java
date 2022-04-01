d28ac05b478f9a31b3f15ec3e1de6308e097dd7f
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-04-01 17:09:10 -0500

    Binary_Medium_371_SumOfTwoIntegers

---
+++ LeetCode/src/Blind75/Binary_Medium_371_SumOfTwoIntegers.java
@@ -0,0 +1,19 @@
+package Blind75;
+
+public class Binary_Medium_371_SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while (a != 0 && b != 0) {
+            int and = a & b;
+            int xor = a ^ b;
+
+            a = and << 1;
+            b = xor;
+        }
+
+        if (a != 0) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
