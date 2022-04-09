1b6652bd4078a78ce2a843f1d778664f30b44a04
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-04-08 22:54:43 -0500

    Prep for ISU CSE competative coding contest 2022

---
+++ CompetativeProgramming/src/ISU_CSE_Contest_2022/Problem1.java
@@ -0,0 +1,24 @@
+package ISU_CSE_Contest_2022;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\MSinnwell\\Projects\\IndependentStudy\\IndependentStudy\\CompetativeProgramming\\src\\ISU_CSE_Contest_2022\\testing.txt");
+
+        Scanner scanner = new Scanner(file);
+
+//        Scanner scanner = new Scanner(System.in);
+
+        int numberOfTestCases = Integer.parseInt(scanner.nextLine());
+
+        int caseNumber = 1;
+
+        for (int i = 0; i < numberOfTestCases; i++) {
+
+        }
+    }
+}
