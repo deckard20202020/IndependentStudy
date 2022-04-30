904f9b9d34baf3ce3ad772b210f1d8dc509ffd87
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-04-30 17:06:09 -0500

    DP_Medium_152_MaximumProductSubarray

--- LeetCode/src/DynamicProgramming/DP_Medium_152_MaximumProductSubarray.java
+++ LeetCode/src/DynamicProgramming/DP_Medium_152_MaximumProductSubarray.java
@@ -3,9 +3,10 @@ package DynamicProgramming;
 public class DP_Medium_152_MaximumProductSubarray {
     public static void main(String[] args) {
 //        int[] nums = {2,3,-2,4};
-//        int[] nums = {-2, 3, -4};
+        int[] nums = {-2, 3, -4};
 //        int[] nums = {2,3,-2,4};
-        int[] nums = {-3,-1,-1};
+//        int[] nums = {-3,-1,-1};
+//        int[] nums = {1, 3, -1, -4, 2, 2};
 
         int answer = maxProduct(nums);
         System.out.println(answer);
@@ -27,16 +28,36 @@ public class DP_Medium_152_MaximumProductSubarray {
         int maxTemp = nums[0];
         int minTemp = nums[0];
 
+        //scroll through the list of numbers
         for (int i = 1; i < nums.length; i++) {
-            int a = nums[i] * maxTemp;
-            int b = nums[i] * minTemp;
+            int a = maxTemp * nums[i];
+            int b = minTemp * nums[i];
 
-            maxTemp = Math.max(Math.max(a,b), nums[i]);
-            minTemp = Math.min(Math.min(a,b), nums[i]);
+            //update our maxTemp
+            maxTemp = Math.max(Math.max(a, b), nums[i]);
+            minTemp = Math.min(Math.min(a, b), nums[i]);
 
-            maxProduct = Math.max(maxProduct,maxTemp);
+            //update our maxProduct
+            maxProduct = Math.max(maxProduct, maxTemp);
         }
 
         return maxProduct;
+
+
+        //This will not work for cases like {-2,3,-4}
+//        //create a dp array
+//        int[] dp = new int[nums.length];
+//        //initialize the first value of the array
+//        dp[0] = nums[0];
+//        //initialize our maxProduct
+//        int maxProduct = nums[0];
+//
+//        for (int i = 1; i < dp.length; i++) {
+//            dp[i] = Math.max(nums[i], dp[i-1] * nums[i]);
+//            maxProduct = Math.max(dp[i] , maxProduct);
+//        }
+//
+//        return maxProduct;
+//    }
     }
 }
