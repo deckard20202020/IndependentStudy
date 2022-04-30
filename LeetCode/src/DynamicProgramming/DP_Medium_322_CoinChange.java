ed8a3c95e34c2cdc5c0164813c0799317cd59011
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-04-30 16:11:39 -0500

    DP_Medium_322_CoinChange

--- LeetCode/src/DynamicProgramming/DP_Medium_322_CoinChange.java
+++ LeetCode/src/DynamicProgramming/DP_Medium_322_CoinChange.java
@@ -1,31 +1,53 @@
 package DynamicProgramming;
 
 public class DP_Medium_322_CoinChange {
-    public int coinChange(int[] coins, int amount) {
+    public static void main(String[] args)  {
+//        int[] coins = {1,2,5};
+//        int amount = 11;
 
-        //this should be a dynammic programming
+        int[] coins = {2};
+        int amount = 3;
 
-        //our dp array rows = coins, columns = total
-        int[][] dp = new int[coins.length][amount + 1];
+        int answer = coinChange(coins, amount);
+        System.out.println(answer);
 
-        //initialize our array
-        for (int i = 0; i < coins.length; i++) {
-            dp[i][0] = 0;
-        }
-        for (int j = 0; j <= amount; j++) {
-            dp[0][j] = j;
-        }
+    }
 
-        //now we will scroll through the array and enter the values
-        for (int row = 1; row < coins.length; row++) {
-            for (int col = 0; col <= amount; ) {
-                //min of (square above) and (1+square in same row - value of coin)
-                dp[row][col] = Math.min(dp[row-1][col], 1 + dp[row][col-coins[col]]);
+    private static int coinChange(int[] coins, int amount) {
+
+        //make an array for our dp
+        int[] dp = new int[amount + 1];
+        //this is initialized to all 0's so we don't need to set the first 0
+
+        //scroll through the dp array
+        //the index of the dp array will be the total we are tyring to make
+        //we can start at 1 because it takes 0 coins to make a total of 0
+        for (int i = 1; i < dp.length; i++) {
+            //initialize the min amount
+            int min = Integer.MAX_VALUE;
+            //scroll through our list of coins
+            for (int j = 0; j < coins.length; j++) {
+                //check 1: the value of our coin is less than
+                //the total we are trying to make
+                if (i - coins[j] >= 0) {
+                    //check 2: we were able to make change for the total
+                    //we are trying to make - the value of the coin we are on
+                    if(dp[i - coins[j]] != Integer.MAX_VALUE) {
+                        //update our min
+                        min = Math.min(min, dp[i - coins[j]] +1);
+                    }
+                }
             }
+            //update our dp array
+            dp[i] = min;
         }
 
-        //we will want the bottom right value
-        return dp[coins.length-1][amount];
+        //make sure we were able to make the change
+        if (dp[amount] == Integer.MAX_VALUE) {
+            return -1;
+        }
 
+        //return our answer
+        return dp[amount];
     }
 }
