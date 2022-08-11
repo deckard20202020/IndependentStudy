69c343be7271d1121624cba9c3b237d70aa03d00
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-08-10 21:08:45 -0500

    IXL

---
+++ IXL/Question2.java
@@ -0,0 +1,15 @@
+package IXL;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\MSinnwell\\Projects\\IndependentStudy\\IndependentStudy\\IXL\\test2.txt");
+        Scanner scanner = new Scanner(file);
+
+//        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ IXL/Question4.java
@@ -0,0 +1,15 @@
+package IXL;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\MSinnwell\\Projects\\IndependentStudy\\IndependentStudy\\IXL\\test4.txt");
+        Scanner scanner = new Scanner(file);
+
+//        Scanner scanner = new Scanner(System.in);
+    }
+}
