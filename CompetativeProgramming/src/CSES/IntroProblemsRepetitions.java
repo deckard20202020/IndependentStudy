a1b593a5fd29da46062f86abc98ea2f67d306c44
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-08-28 14:25:28 -0500

    IntroProblemsRepetitions

---
+++ CompetativeProgramming/src/CSES/IntroProblemsRepetitions.java
@@ -0,0 +1,33 @@
+//package CSES;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class IntroProblemsRepetitions {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //File file = new File("C:\\Users\\MSinnwell\\Projects\\IndependentStudy\\IndependentStudy\\CompetativeProgramming\\src\\CSES\\test.txt");
+
+        //Scanner scanner = new Scanner(file);
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+
+        char curr = string.charAt(0);
+        int max = 0;
+        int counter = 1;
+
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i) == curr) {
+                counter++;
+            } else {
+                max = Math.max(max, counter);
+                curr = string.charAt(i);
+                counter = 1;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
