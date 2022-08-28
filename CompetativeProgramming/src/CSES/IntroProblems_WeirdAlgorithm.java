3ff7cb6b54253d9af752c7e02e4789c2a5ac9097
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-08-28 13:37:48 -0500

    _IntroProblems_WeirdAlgorithm

---
+++ CompetativeProgramming/src/CSES/IntroProblems_WeirdAlgorithm.java
@@ -0,0 +1,43 @@
+package CSES;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IntroProblems_WeirdAlgorithm {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\MSinnwell\\Projects\\IndependentStudy\\IndependentStudy\\CompetativeProgramming\\src\\CSES\\test.txt");
+
+        Scanner scanner = new Scanner(file);
+
+        String nString = scanner.nextLine();
+
+        int n = Integer.valueOf(nString);
+
+        List<Integer> answer = findPath(n);
+
+        for (Integer num : answer) {
+            System.out.print(num + " ");
+        }
+    }
+
+    private static List<Integer> findPath(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(n);
+
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                list.add(n);
+            } else {
+                n = (n * 3) + 1;
+                list.add(n);
+            }
+        }
+
+        return list;
+    }
+}
