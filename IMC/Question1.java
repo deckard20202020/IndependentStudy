1339ad6c55dff78e5df98d3f0e4f7874091acb01
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-09-24 15:53:45 -0500

    IMC practice 1

--- IMC/Question1.java
+++ IMC/Question1.java
@@ -23,12 +23,13 @@ public class Question1 {
             return "No";
         }
 
-        int[][] cache = new int[1001][1001];
-        for (int[] array : cache) {
-            Arrays.fill(array, -1);
-        }
+//        int[][] cache = new int[1001][1001];
+//        for (int[] array : cache) {
+//            Arrays.fill(array, -1);
+//        }
 
-        boolean canReach = findPath(c, x1, x2, y1, y2, cache);
+//        boolean canReach = findPath(c, x1, x2, y1, y2, cache);
+        boolean canReach = findPath(c, x1, x2, y1, y2);
         if (canReach) {
             return "Yes";
         }
@@ -36,7 +37,8 @@ public class Question1 {
         return "No";
     }
 
-    private static boolean findPath(int c, int x1, int x2, int y1, int y2, int[][] cache) {
+    private static boolean findPath(int c, int x1, int x2, int y1, int y2) {
+//    private static boolean findPath(int c, int x1, int x2, int y1, int y2, int[][] cache) {
 
         if (x1 > x2 || y1 > y2) {
             return false;
@@ -45,96 +47,142 @@ public class Question1 {
             return true;
         }
 
-        if (dfs(c, x1, x2, y1, y2, cache) != -1) {
-            return true;
-        }
-
-        return false;
-
-//        Set<Pair> visited = new HashSet<>();
-//        Stack<Pair> stack = new Stack<>();
-//        Pair pair = new Pair(x1, y1);
-//        stack.push(pair);
-////        visited.add(pair);
-//
-//        while (!stack.isEmpty()) {
-//            Pair p = stack.pop();
-//
-//            if (p.x == x2 && p.y == y2) {
-//                return true;
-//            }
-//
-//            if (p.x > x2 || p.y > y2) {
-//                visited.add(p);
-//                continue;
-//            }
-//
-//            if (!visited.contains(p)) {
-////            if (!p.visited) {
-//                //find the neighbors
-//                addNeighbors(stack, p, c);
-//            }
-//
-//            visited.add(p);
-//            p.visited = true;
+//        if (dfs(c, x1, x2, y1, y2, cache) != -1) {
+//            return true;
 //        }
 //
 //        return false;
-//    }
 
-//    private static void addNeighbors(Stack<Pair> stack, Pair p, int c) {
-//
-//        Pair p1 = new Pair(p.x + p.y, p.y);
-//        if (p1.x >= p.x && p1.y >= p.y) {
-//            stack.add(p1);
-//        }
-//        Pair p2 = new Pair(p.x, p.x + p.y);
-//        if (p2.x >= p.x && p2.y >= p.y) {
-//            stack.add(p1);
-//        }
-//        Pair p3 = new Pair(p.x + c, p.y + c);
-//        if (p3.x >= p.x && p3.y >= p.y) {
-//            stack.add(p1);
-//        }
-//
-//    }
+        Set<Pair> visited = new HashSet<>();
+        Stack<Pair> stack = new Stack<>();
+        Pair pair = new Pair(x1, y1);
+        stack.push(pair);
 
-//    private static class Pair {
-//
-//        int x;
-//        int y;
-//        boolean visited;
-//
-//        public Pair(int x, int y) {
-//            this.x = x;
-//            this.y = y;
-//            visited = false;
-//        }
+        while (!stack.isEmpty()) {
+            Pair p = stack.pop();
+
+            if (p.x == x2 && p.y == y2) {
+                return true;
+            }
+
+            if (p.x > x2 || p.y > y2) {
+                visited.add(p);
+                continue;
+            }
+
+            if (!visited.contains(p)) {
+                //find the neighbors
+                addNeighbors(stack, p, c, x2, y2);
+            }
+
+            visited.add(p);
+        }
+
+        return false;
     }
 
-    private static int dfs(int c, int x1, int x2, int y1, int y2, int[][] cache) {
+    private static void addNeighbors(Stack<Pair> stack, Pair p, int c, int x2, int y2) {
 
-        if (cache[x1][y1] != -1) {
-            return cache[x1][y1];
+        boolean perfectSquare = false;
+        double number = 0;
+        //right
+        Pair p1 = new Pair(p.x + p.y, p.y);
+        number = p1.x + p1.y;
+        perfectSquare = isPerfect(number);
+        if (!perfectSquare) {
+            if (p1.x <= x2 && p1.y <= y2) {
+                stack.add(p1);
+            }
         }
 
-        if (Math.sqrt(x1 + y1) % 1 == 0) {
-            return 0;
-        }
-        if (x1 > x2 || y1 > y2) {
-            return 0;
-        }
-
-        int constant = dfs(c, x1 + c, y1 + c, x2, y2, cache);
-        int right = dfs(c, x1 + y1, y1, x2, y2, cache);
-        int up = dfs(c, x1, y1 + x1, x2, y2, cache);
-
-        if (constant == 1 || right == 1 || up == 1) {
-            cache[x1][y1] = 1;
-        } else {
-            cache[x1][y1] = 0;
+        //up
+        Pair p2 = new Pair(p.x, p.x + p.y);
+        number = p2.x + p2.y;
+        perfectSquare = isPerfect(number);
+        if (!perfectSquare) {
+            if (p2.x <= x2 && p2.y <= y2) {
+                stack.add(p2);
+            }
         }
 
-        return cache[x1][y1];
+        //constant
+        Pair p3 = new Pair(p.x + c, p.y + c);
+        number = p3.x + p3.y;
+        perfectSquare = isPerfect(number);
+        if (!perfectSquare) {
+            if (p3.x <= x2 && p3.y <= y2) {
+                stack.add(p3);
+            }
+        }
     }
+
+    private static boolean isPerfect(double number) {
+
+        double sqareRoot = Math.sqrt(number);
+
+        return((sqareRoot - Math.floor(sqareRoot)) == 0);
+    }
+
+    private static class Pair {
+
+        int x;
+        int y;
+        int hashCode;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.hashCode = Objects.hash(x, y);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+
+            if (this == o) {
+                return true;
+            }
+
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Pair p = (Pair) o;
+            if (this.x == p.x && this.y == p.y) {
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.hashCode;
+        }
+    }
+
+//    private static int dfs(int c, int x1, int x2, int y1, int y2, int[][] cache) {
+//
+//        if (cache[x1][y1] != -1) {
+//            return cache[x1][y1];
+//        }
+//
+//        if (Math.sqrt(x1 + y1) % 1 == 0) {
+//            return 0;
+//        }
+//        if (x1 > x2 || y1 > y2) {
+//            return 0;
+//        }
+//
+//        int constant = dfs(c, x1 + c, y1 + c, x2, y2, cache);
+//        int right = dfs(c, x1 + y1, y1, x2, y2, cache);
+//        int up = dfs(c, x1, y1 + x1, x2, y2, cache);
+//
+//        if (constant == 1 || right == 1 || up == 1) {
+//            cache[x1][y1] = 1;
+//        } else {
+//            cache[x1][y1] = 0;
+//        }
+//
+//        return cache[x1][y1];
+//    }
 }
--- IMC/Question2.java
+++ IMC/Question2.java
@@ -127,7 +127,7 @@ public class Question2 {
 
             currentTime++;
         }
-        
+
         return answer;
     }
 }
