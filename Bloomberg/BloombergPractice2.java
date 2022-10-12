bbd2a188fdc51216d753a7417b7a23622730fab7
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-10-11 21:49:57 -0500

    BloombergPractice2

---
+++ Bloomberg/BloombergPractice2.java
@@ -0,0 +1,15 @@
+package Bloomberg;
+
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BloombergPractice2 {
+    public static void main(String[] args) {
+        String[] words = {"hello", "hello", "hi"};
+        PriorityQueue<String> q = new PriorityQueue<>();
+        Stack<Integer> s = new Stack<>();
+        Queue<Integer> q2 = new ArrayDeque<>();
+    }
+}
