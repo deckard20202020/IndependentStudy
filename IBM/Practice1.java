aa451faaaf883607bc9bc737795e68905a4dddbc
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-10-30 20:26:59 -0500

    IMB Practice1

--- IBM/Practice1.java
+++ IBM/Practice1.java
@@ -17,33 +17,42 @@ public class Practice1 {
 
     private static int[] getFinalData(int n, int[] data, int[][] updates) {
 
-        int[] result = new int[data.length];
-
-        //make a hashmap that is 1 indexed
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < data.length; i++) {
-            int key = i + 1;
-            int value = data[i];
-            map.put(key, value);
-        }
-
         for (int[] arr : updates) {
             int start = arr[0];
             int end = arr[1];
-            for (int i = start; i <= end; i++) {
-                int key = i;
-                int value = map.get(key);
-                value = -value;
-                map.put(key, value);
+            for (int i = start - 1; i < end; i++) {
+                data[i] = -data[i];
             }
         }
 
-        for (Integer key : map.keySet()) {
-            int index = key - 1;
-            int value = map.get(key);
-            result[index] = value;
-        }
-
-        return result;
+        return data;
+//        int[] result = new int[data.length];
+//
+//        //make a hashmap that is 1 indexed
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < data.length; i++) {
+//            int key = i + 1;
+//            int value = data[i];
+//            map.put(key, value);
+//        }
+//
+//        for (int[] arr : updates) {
+//            int start = arr[0];
+//            int end = arr[1];
+//            for (int i = start; i <= end; i++) {
+//                int key = i;
+//                int value = map.get(key);
+//                value = -value;
+//                map.put(key, value);
+//            }
+//        }
+//
+//        for (Integer key : map.keySet()) {
+//            int index = key - 1;
+//            int value = map.get(key);
+//            result[index] = value;
+//        }
+//
+//        return result;
     }
 }
