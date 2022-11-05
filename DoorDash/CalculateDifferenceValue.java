1ab26257e997c794c5fa7151761c44251d4f15f0
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-11-05 14:27:46 -0500

    CalculateDifferenceValue

---
+++ DoorDash/CalculateDifferenceValue.java
@@ -0,0 +1,27 @@
+package DoorDash;
+
+public class CalculateDifferenceValue {
+    public static void main(String[] args) {
+
+        //How should I do this one???
+
+        String firstString = "HACKERRANK";
+        String secondString = "HACKERMAN";
+
+        int answer = findDifferenceValue(firstString, secondString);
+        System.out.println(answer);
+    }
+
+    private static int findDifferenceValue(String firstString, String secondString) {
+
+        int answer = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+
+
+        return max - min + 1;
+    }
+
+
+}
