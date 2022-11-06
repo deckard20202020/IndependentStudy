67dd5199d082af195534eea8533afb5d1544546a
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-11-06 01:01:40 -0500

    CalculateDifference

--- DoorDash/CalculateDifferenceValue.java
+++ DoorDash/CalculateDifferenceValue.java
@@ -7,6 +7,7 @@ public class CalculateDifferenceValue {
 
         String firstString = "HACKERRANK";
         String secondString = "HACKERMAN";
+//        String secondString = "LACKERRAND";
 
         int answer = findDifferenceValue(firstString, secondString);
         System.out.println(answer);
@@ -18,34 +19,22 @@ public class CalculateDifferenceValue {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
-//        I think we can solve it in O(n):
-//        for every i in firstString[] find the maximum prefix of secondString[] that appears as a subsequence to the left of i,
-//        and the maximum suffix of secondString[] that appears as a subsequence to the right of i.
-//        call them maxPrefix[i] and maxSuffix[i]
-//        Then to get the answer, you would take the min(ans, max(0, maxSuffix[i] - maxPrefix[i]))
-//        just the minimum difference over all i
-//        wait no this is actually min(ans, max(0, secontString.length - maxSuffix[i] - maxPrefix[i]))
 
         for (int i = 0; i < firstString.length(); i++) {
             int maxPost = 0;
             int maxPre = 0;
             //find max prefix of secondString that appears as a subsequence to the left of i
-            for (int j = 0; j < i; j++) {
-                maxPre = maxPrefix(firstString, secondString, i);
-            }
+            maxPre = maxPrefix(firstString, secondString, i);
 
             //find max suffix of secondString that appears as a subsequence to the right of i
-            for (int k = i; k < secondString.length(); k++) {
-                maxPost = maxPostfix(firstString, secondString, i);
-            }
+            maxPost = maxPostfix(firstString, secondString, i);
 
-            answer = Math.min(answer, Math.max(0, maxPost - maxPre));
-//            answer = Math.min(answer, Math.max(0, secondString.length() - maxPost - maxPre));
+//            answer = Math.min(answer, Math.max(0, maxPost - maxPre));
+            answer = Math.min(answer, Math.max(0, secondString.length() - maxPost - maxPre));
         }
 
 
             return answer + 1;
-//        return max - min + 1;
     }
 
     static int maxPrefix(String s, String t, int index) {
