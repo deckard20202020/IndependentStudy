5b3c53ae8adc9abe08057f808c4e6ce1e37a705d
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-11-06 17:23:41 -0600

    SubstringRemoval

---
+++ IBM/SubstringRemoval.java
@@ -0,0 +1,39 @@
+package IBM;
+
+public class SubstringRemoval {
+
+    public static void main(String[] args) {
+
+//        String seq = "BBABBA";
+        String seq = "AABBBAB";
+        int answer = getMinLength(seq);
+        System.out.println(answer);
+
+    }
+
+    public static int getMinLength(String seq) {
+
+        int answer = seq.length();
+
+        answer = helper(seq, answer);
+        return answer;
+    }
+
+    private static int helper(String seq, int answer) {
+
+        int ab = 0;
+        int bb = 0;
+        if (seq.contains("AB")) {
+            seq = seq.replace("AB", "");
+            ab = helper(seq, seq.length());
+        }
+
+        if (seq.contains("BB")) {
+            seq = seq.replace("BB", "");
+            bb = helper(seq, seq.length());
+        }
+
+        return Math.min(seq.length(), Math.min(ab, bb));
+
+    }
+}
