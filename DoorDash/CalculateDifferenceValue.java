4e62e10e4fabfa464b65385c3b4d53baa85e38f1
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-11-06 15:18:50 -0600

    CalculateDifferenceValue

--- DoorDash/CalculateDifferenceValue.java
+++ DoorDash/CalculateDifferenceValue.java
@@ -6,74 +6,56 @@ public class CalculateDifferenceValue {
         //How should I do this one???
 
         String firstString = "HACKERRANK";
-        String secondString = "HACKERMAN";
-//        String secondString = "LACKERRAND";
+//        String secondString = "HACKERMAN";
+        String secondString = "LACKERRAND";
 
         int answer = findDifferenceValue(firstString, secondString);
         System.out.println(answer);
     }
 
     private static int findDifferenceValue(String firstString, String secondString) {
-
         int answer = Integer.MAX_VALUE;
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
+        int n = firstString.length();
+        int m = secondString.length();
 
-        for (int i = 0; i < firstString.length(); i++) {
-            int maxPost = 0;
-            int maxPre = 0;
-            //find max prefix of secondString that appears as a subsequence to the left of i
-            maxPre = maxPrefix(firstString, secondString, i);
+        int[] maxPrefix = new int[n];
+        int[] maxSuffix = new int[n];
 
-            //find max suffix of secondString that appears as a subsequence to the right of i
-            maxPost = maxPostfix(firstString, secondString, i);
-
-//            answer = Math.min(answer, Math.max(0, maxPost - maxPre));
-            answer = Math.min(answer, Math.max(0, secondString.length() - maxPost - maxPre));
+        // calculate max prefix of secondString in firstString
+        // maxPrefix[i] holds the index of the rightmost character in secondString tht doesn't match in firstString (as a subsequence)
+        int p = 0;
+        for(int i=0; i<n; i++){
+            if(p < m && firstString.charAt(i) == secondString.charAt(p)){
+                p++;
+            }
+            maxPrefix[i] = p;
         }
 
-
-            return answer + 1;
-    }
-
-    static int maxPrefix(String s, String t, int index) {
-        int count = 0;
-
-        // Iterating string T.
-        for (int i = 0; i < index; i++)
-        {
-            // If end of string S.
-            if (count == s.length())
-                break;
-
-            // If character match,
-            // increment counter.
-            if (t.charAt(i) == s.charAt(count))
-                count++;
+        // calculate max suffix of secondString in firstString
+        // maxSuffix[i] holds the index of the leftmost character in secondString that doesn't match in firstString (as a subsequence)
+        p = m-1;
+        for(int i=n-1; i>=0; i--){
+            if(p >= 0 && firstString.charAt(i) == secondString.charAt(p)){
+                p--;
+            }
+            maxSuffix[i] = p;
         }
 
-        return count;
-    }
-
-    static int maxPostfix(String s, String t, int index) {
-        int count = 0;
-
-        // Iterating string T.
-        for (int i = index; i < t.length(); i++)
-        {
-            // If end of string S.
-            if (count == s.length())
-                break;
-
-            // If character match,
-            // increment counter.
-            if (t.charAt(i) == s.charAt(count))
-                count++;
+        // now that we have the maxPrefixes and maxSuffixes, we just need to iterate over the firstString
+        // and imagine a 'splitter' between every two indices, so that to the left of the splitter we want to imagine the maxPrefix
+        // of the second string and to the right of the splitter we want to imagine the maxSuffix of the secondString
+        for(int i=0; i<n-1; i++){
+            answer = Math.min(answer, Math.max(0, maxSuffix[i+1] - maxPrefix[i] + 1));
         }
 
-        return count;
-    }
+        // here you can imagine the splitter to be at (1) rightBefore the first character in firstString (hence, maxSuffix[0]+1)
+        // and (2) right after the last character in firstString (hence, maxPrefix[i-1])
+        answer = Math.min(answer, Math.min(maxSuffix[0]+1, m - maxPrefix[n-1]));
 
+        return answer;
+    }
 
 }
