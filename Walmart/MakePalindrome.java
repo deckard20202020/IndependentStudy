cd83a171ffdc05f0c9f6ae67ca9286229b52fe44
Matt Sinnwell <mattdsinnwell@gmail.com>
2022-12-16 20:38:57 -0600

    make palindrome

--- Walmart/MakePalindrome.java
+++ Walmart/MakePalindrome.java
@@ -5,23 +5,26 @@ import java.util.*;
 public class MakePalindrome {
     public static void main(String[] args) {
 
-        String string = "aabc";
-//        String string = "aabcd";
+//        String string = "aabc";
+        String string = "aabcd";
         String answer = palindrome(string);
         System.out.println(answer);
 
     }
 
-    static int N = 200100;
-    static int M = 26;
-    static char[] s = new char[N];
-    static int[] freq = new int[M];
-    static int[] odd = new int[M];
+//    static int N = 200100;
+//    static int M = 26;
+//    static char[] s = new char[N];
+//    static int[] freq = new int[26];
+//    static int[] odd = new int[26];
 
     public static String palindrome(String string) {
 
+        int[] freq = new int[26];
+        int[] odd = new int[26];
+
         String answer = "";
-        
+
         int n = string.length();
         int odd_count = 0;
         int odd_mid = 0;
@@ -29,7 +32,7 @@ public class MakePalindrome {
 //            ++freq[s[i] - 'a'];
             freq[Integer.valueOf(string.charAt(i) - 'a')]++;
         }
-        for (int i = 0; i < M; ++i) {
+        for (int i = 0; i < 26; ++i) {
             if (freq[i] % 2 == 1) {
                 odd[odd_count] = i;
                 ++odd_count;
@@ -44,7 +47,7 @@ public class MakePalindrome {
                 odd_mid = odd[i];
             }
         }
-        for (int i = 0; i < M; ++i) {
+        for (int i = 0; i < 26; ++i) {
             for (int j = 0; j < freq[i] / 2; ++j) {
                 int letterInt = 'a' + i;
                 char c = (char)letterInt;
@@ -57,7 +60,7 @@ public class MakePalindrome {
             answer = answer + c;
 //            System.out.print('a' + odd_mid);
         }
-        for (int i = M - 1; i >= 0; --i) {
+        for (int i = 26 - 1; i >= 0; --i) {
             for (int j = 0; j < freq[i] / 2; ++j) {
                 int letterInt = 'a' + i;
                 char c = (char)letterInt;
